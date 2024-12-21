eda07cbe618cfcbe3adb3b472581c92aa63b4582
Serge Matveenko <lig@countzero.co>
2024-12-21 21:58:47 +0100

    Implement calling actual commands in generator

--- src/generator.rs
+++ src/generator.rs
@@ -1,5 +1,7 @@
-use std::thread;
 use std::time::Duration;
+use std::{process::Command, thread};
+
+use serde::Serialize;
 
 use crate::{
     config::Config,
@@ -10,6 +12,7 @@ pub struct StatusGenerator {
     interval: Duration,
     header_sent: bool,
     config: Config,
+    pretty: bool,
 }
 
 impl StatusGenerator {
@@ -18,6 +21,14 @@ impl StatusGenerator {
             interval: Duration::from_secs(1),
             header_sent: false,
             config,
+            pretty: false,
+        }
+    }
+
+    fn to_json<T: Serialize>(&self, value: T) -> String {
+        match self.pretty {
+            true => serde_json::to_string_pretty(&value).unwrap(),
+            false => serde_json::to_string(&value).unwrap(),
         }
     }
 }
@@ -28,25 +39,33 @@ impl Iterator for StatusGenerator {
     fn next(&mut self) -> Option<Self::Item> {
         if !self.header_sent {
             self.header_sent = true;
-            return Some(format!(
-                "{}\n[",
-                serde_json::to_string(&Header::new()).unwrap()
-            ));
+            return Some(format!("{}\n[", self.to_json(Header::new())));
         }
 
         thread::sleep(self.interval);
 
         let mut blocks: Vec<Block> = vec![];
+
         for section in &self.config.sections {
-            blocks.push(Block::new(
-                &section.command,
-                &section.command,
-                "command",
-                &section.name,
-            ));
+            let output = Command::new("sh")
+                .args(["-c", &section.command])
+                .output()
+                .unwrap_or_else(|_| panic!("Failed to execute command `{}`", &section.command));
+
+            if !output.status.success() {
+                panic!(
+                    "Command `{}` failed with error:\n{}",
+                    &section.command,
+                    String::from_utf8_lossy(&output.stderr)
+                );
+            }
+
+            let stdout = String::from_utf8_lossy(output.stdout.trim_ascii_end());
+
+            blocks.push(Block::new(&stdout, &stdout, "command", &section.name));
         }
 
-        Some(serde_json::to_string(&Status { blocks }).unwrap())
+        Some(self.to_json(&Status { blocks }))
     }
 }
 
@@ -60,6 +79,18 @@ mod tests {
     use super::StatusGenerator;
 
     #[rstest]
+    fn should_have_new_line_after_header() {
+        let mut status_generator = StatusGenerator::new(Config { sections: vec![] });
+
+        let header = status_generator.next().unwrap();
+        assert_eq!(
+            header,
+            r#"{"version":1,"click_events":false,"cont_signal":18,"stop_signal":19}
+["#
+        );
+    }
+
+    #[rstest]
     fn should_generate_status() {
         let mut status_generator = StatusGenerator::new(Config {
             sections: vec![
@@ -69,26 +100,67 @@ mod tests {
                 },
                 Section {
                     name: "second section".to_string(),
-                    command: r#"date "+%Y-%m-%d %H:%M:%S""#.to_string(),
+                    command: r#"date -d "2024-12-21 21:44:54" "+%Y-%m-%d %H:%M:%S""#.to_string(),
                 },
             ],
         });
+        status_generator.pretty = true;
 
         let header = status_generator.next().unwrap();
         assert_eq!(
             header,
-            r#"{"version":1,"click_events":false,"cont_signal":18,"stop_signal":19}
+            r#"{
+  "version": 1,
+  "click_events": false,
+  "cont_signal": 18,
+  "stop_signal": 19
+}
 ["#
         );
 
-        let statuses: Vec<String> = status_generator.take(3).collect();
+        let status1: String = status_generator.next().unwrap();
         assert_eq!(
-            statuses,
-            vec![
-                r##"[{"full_text":"uname -r","short_text":"uname -r","color":"#000000","background":"#ffffff","border":"#000000","border_top":1,"border_bottom":1,"border_left":1,"border_right":1,"min_width":"uname -r","align":"left","name":"command","instance":"first section","urgent":false,"separator":true,"separator_block_width":9,"markup":"none"},{"full_text":"date \"+%Y-%m-%d %H:%M:%S\"","short_text":"date \"+%Y-%m-%d %H:%M:%S\"","color":"#000000","background":"#ffffff","border":"#000000","border_top":1,"border_bottom":1,"border_left":1,"border_right":1,"min_width":"date \"+%Y-%m-%d %H:%M:%S\"","align":"left","name":"command","instance":"second section","urgent":false,"separator":true,"separator_block_width":9,"markup":"none"}]"##,
-                r##"[{"full_text":"uname -r","short_text":"uname -r","color":"#000000","background":"#ffffff","border":"#000000","border_top":1,"border_bottom":1,"border_left":1,"border_right":1,"min_width":"uname -r","align":"left","name":"command","instance":"first section","urgent":false,"separator":true,"separator_block_width":9,"markup":"none"},{"full_text":"date \"+%Y-%m-%d %H:%M:%S\"","short_text":"date \"+%Y-%m-%d %H:%M:%S\"","color":"#000000","background":"#ffffff","border":"#000000","border_top":1,"border_bottom":1,"border_left":1,"border_right":1,"min_width":"date \"+%Y-%m-%d %H:%M:%S\"","align":"left","name":"command","instance":"second section","urgent":false,"separator":true,"separator_block_width":9,"markup":"none"}]"##,
-                r##"[{"full_text":"uname -r","short_text":"uname -r","color":"#000000","background":"#ffffff","border":"#000000","border_top":1,"border_bottom":1,"border_left":1,"border_right":1,"min_width":"uname -r","align":"left","name":"command","instance":"first section","urgent":false,"separator":true,"separator_block_width":9,"markup":"none"},{"full_text":"date \"+%Y-%m-%d %H:%M:%S\"","short_text":"date \"+%Y-%m-%d %H:%M:%S\"","color":"#000000","background":"#ffffff","border":"#000000","border_top":1,"border_bottom":1,"border_left":1,"border_right":1,"min_width":"date \"+%Y-%m-%d %H:%M:%S\"","align":"left","name":"command","instance":"second section","urgent":false,"separator":true,"separator_block_width":9,"markup":"none"}]"##,
-            ]
+            status1,
+            r##"[
+  {
+    "full_text": "6.12.5-200.fc41.x86_64",
+    "short_text": "6.12.5-200.fc41.x86_64",
+    "color": "#000000",
+    "background": "#ffffff",
+    "border": "#000000",
+    "border_top": 1,
+    "border_bottom": 1,
+    "border_left": 1,
+    "border_right": 1,
+    "min_width": "6.12.5-200.fc41.x86_64",
+    "align": "left",
+    "name": "command",
+    "instance": "first section",
+    "urgent": false,
+    "separator": true,
+    "separator_block_width": 9,
+    "markup": "none"
+  },
+  {
+    "full_text": "2024-12-21 21:44:54",
+    "short_text": "2024-12-21 21:44:54",
+    "color": "#000000",
+    "background": "#ffffff",
+    "border": "#000000",
+    "border_top": 1,
+    "border_bottom": 1,
+    "border_left": 1,
+    "border_right": 1,
+    "min_width": "2024-12-21 21:44:54",
+    "align": "left",
+    "name": "command",
+    "instance": "second section",
+    "urgent": false,
+    "separator": true,
+    "separator_block_width": 9,
+    "markup": "none"
+  }
+]"##,
         );
     }
 }
